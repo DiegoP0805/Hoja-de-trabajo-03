1880a88eabb2b6da90109f35544fa6a7d0ce44ba
stefanquintana <77856209+stefanquintana@users.noreply.github.com>
2021-02-15 10:21:20 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Sorts m_sort = new Sorts();
+		ArrayList<Integer> arreglo2 = new ArrayList<Integer>(Arrays.asList(10,128,10008,34,56,78,43,21,39,11,13));
+		
+		// starting time 
+        long start = System.currentTimeMillis(); 
+       
+        // start of function 
+  
+        m_sort.merge_sort(arreglo2); 
+  
+        // end of function 
+  
+        // ending time 
+        long end = System.currentTimeMillis(); 
+        System.out.println((end - start) + "ms"); 
+    } 
+  
+    // A dummy function that runs a loop x times 
+    public static void count_function(long x) 
+    { 
+        System.out.println("Loop starts"); 
+        for (long i = 0; i < x; i++) 
+            ; 
+        System.out.println("Loop ends"); 
+    } 
+
+}
